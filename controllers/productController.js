d16afbbf73cb7983fb838ffcf6b62b180fca6532
Niaz <niazulalam097@gmail.com>
2026-02-11 16:27:41 +0600

    Update and delete product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,8 @@
 const productSchema = require("../model/productSchema");
 
 async function productController(req, res) {
-  const { name, description, category, price, size, color, ram, storage } = req.body;
+  const { name, description, category, price, size, color, ram, storage } =
+    req.body;
   const createProduct = new productSchema({
     name,
     description,
@@ -19,4 +20,58 @@ async function productController(req, res) {
   });
 }
 
-module.exports = productController;
+async function getProductController(req, res) {
+  const getProduct = await productSchema.find({});
+  res.json({
+    message: "Success",
+    data: getProduct,
+  });
+}
+
+async function updateProductController(req, res) {
+  const { id } = req.params;
+  const { name, description, category, price, size, color, ram, storage } =
+    req.body;
+  const updateProduct = await productSchema.findById(id);
+  updateProduct.name = name;
+  updateProduct.description = description;
+  updateProduct.category = category;
+  updateProduct.price = price;
+  updateProduct.size = size;
+  updateProduct.color = color;
+  updateProduct.ram = ram;
+  updateProduct.storage = storage;
+
+  await updateProduct.save();
+
+  res.json({
+    message: "Success",
+    data: updateProduct,
+  });
+}
+
+async function deleteProduct(req, res) {
+  const { id } = req.params;
+  const deleteProduct = await productSchema.findByIdAndDelete(id);
+
+  res.json({
+    message: "Successfully deleted",
+    data: deleteProduct,
+  });
+}
+
+async function deleteAllProduct(req, res) {
+  const deleteAllProduct = await productSchema.deleteMany({});
+  res.json({
+    message: "Successfully deleted",
+    data: deleteAllProduct,
+  });
+}
+
+module.exports = {
+  productController,
+  updateProductController,
+  getProductController,
+  deleteProduct,
+  deleteAllProduct,
+};
--- route/api/product.js
+++ route/api/product.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const productController = require('../../controllers/productController')
+const express = require("express");
+const router = express.Router();
+const {
+  productController,
+  updateProductController,
+  getProductController,
+  deleteProduct,
+  deleteAllProduct,
+} = require("../../controllers/productController");
 
+router.post("/createproduct", productController);
+router.get("/getproduct", getProductController);
+router.patch("/updateproduct/:id", updateProductController);
+router.delete("/deleteproduct/:id", deleteProduct);
+router.delete("/deleteallproduct", deleteAllProduct);
 
-
-router.post("/createproduct", productController)
-
-module.exports = router
+module.exports = router;
