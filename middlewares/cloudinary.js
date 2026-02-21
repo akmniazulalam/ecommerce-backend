fe386d734864f515e6bfc8c73ed1360cf1ba1c21
Niaz <niazulalam097@gmail.com>
2026-02-21 14:56:22 +0600

    Cloudinary install and configuration

---
+++ middlewares/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: 'dvhjuwdac',
+  api_key: '215332654421494',
+  api_secret: 'piDmJz97Ts4j12BsXdv_3-YL03k'
+});
+
+module.exports = cloudinary
